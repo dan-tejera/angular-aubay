5473bb24e2fe67fef4267fe3e8865088c67f3026
dtejera <dtejera@aubay.es>
2019-07-18 12:59:40 +0200

    Modificaciones iniciales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-aubay';
+  title = 'Hola Angular Aubay';
+  logo = '../assets/AngularLogo.svg';
 }
