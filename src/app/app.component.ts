c30b347b1ce8ae199691b8a7f3bd7d450fc065b7
dtejera <dtejera@aubay.es>
2019-07-22 10:30:17 +0200

    Modulos y Componentes Core

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hola Angular Aubay';
-  logo = '../assets/AngularLogo.svg';
+
+    constructor() {
+      
+    }
 }
---
+++ src/app/core/footer/footer.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'aby-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  autor: string;
+  empresa: string;
+  fecha: Date;
+
+  constructor() { }
+
+  ngOnInit() {
+
+    this.autor = 'Daniel Tejera';
+    this.empresa = 'Aubay';
+    this.fecha = new Date();
+    
+  }
+
+}
